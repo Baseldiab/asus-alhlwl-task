00ab8e3360a5cc855288b987b16d58299c81b61a
Basel Diab <114107867+Baseldiab@users.noreply.github.com>
2024-08-27 01:25:38 +0300

    fix build error

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,17 +1,8 @@
-import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
     <>
-      <Helmet>
-        <title>E-commerce Techno | Page Not Found</title>
-        <meta
-          name="description"
-          content="The page you are looking for does not exist. Check out our other products and categories at E-commerce Techno."
-        />
-      </Helmet>
-
       <section className="fixed inset-0 flex items-center justify-center w-screen h-screen">
         <div className="px-4 lg:py-12">
           <div className="lg:gap-4 lg:flex">
